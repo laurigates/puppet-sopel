814e67710126f4cca96178395c0d7421e60968f6
Lauri Gates <lauri.gates@frozenbyte.com>
2019-07-02 13:35:53 +0300

    Fix rubocop complaint

--- spec/classes/sopel_spec.rb
+++ spec/classes/sopel_spec.rb
@@ -13,21 +13,22 @@ describe 'sopel' do
       when 'Debian'
         case os_facts[:lsbmajdistrelease]
         when '10'
-          it { is_expected.to contain_class('python').with_version('3.7') }
+          python_version = '3.7'
         when '9'
-          it { is_expected.to contain_class('python').with_version('3.5') }
+          python_version = '3.5'
         when '8'
-          it { is_expected.to contain_class('python').with_version('3.4') }
+          python_version = '3.4'
         end
       when 'Ubuntu'
         case os_facts[:lsbmajdistrelease]
         when '18.04'
-          it { is_expected.to contain_class('python').with_version('3.7') }
+          python_version = '3.7'
         when '16.04'
-          it { is_expected.to contain_class('python').with_version('3.5') }
+          python_version = '3.5'
         end
       end
 
+      it { is_expected.to contain_class('python').with_version(python_version.to_s) }
       it { is_expected.to contain_class('python') }
       it { is_expected.to contain_package('python-enchant') }
       it { is_expected.to contain_python__pip('sopel') }
